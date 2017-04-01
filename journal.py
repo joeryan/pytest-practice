853627f2b8e0fdf6804fbac537728bca8ee562ce
Joe Ryan <joe@clanryan.us>
2017-03-31 23:23:03 -0400

    added persistence using text file

--- journal.py
+++ journal.py
@@ -1,11 +1,21 @@
 import sys
+from pathlib import Path
 
-
+jfile = Path('./data/default.jrn')
 print('-'*40 + "\n")
 print("\t\tJOURNAL APP")
 print('-'*40 + "\n")
 
 journal = []
+
+if jfile.is_file():
+	count = 0
+	print("... loading journal from ./data/default.jrn ...")
+	with open(jfile) as jf:
+		for entry in jf.readlines():
+			journal.append(entry.strip())
+			count += 1
+	print("... loaded {c} journal entries ...".format(c=count))
 
 def list_entries():
 	count = 1
@@ -22,3 +32,11 @@ while True:
 		journal.append(entry)
 	if choice.upper() == "X":
 		break
+
+print("... saving journal to ./data/default.jrn ...")
+count = 0
+with open(jfile, 'w') as jf:
+	for entry in journal:
+		jf.write(entry + "\n")
+		count += 1
+print("... saved {c} journal entries ...".format(c=count))
