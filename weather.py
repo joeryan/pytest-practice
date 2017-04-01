94905e787066c38776959f558c0fe37328899bb1
Joe Ryan <joe@clanryan.us>
2017-04-01 00:08:47 -0400

    added api call and api key (no parsing of response)

--- weather.py
+++ weather.py
@@ -3,12 +3,14 @@
 # a simple app that gets the weather for a zip code entered at command line
 import re
 import requests
+from os import environ as env
 
 
 print('-'*40)
 print("\tWEATHER CLIENT APP")
 print('-'*40 + "\n")
 
+apiKey = env['OWM_KEY'] if 'OWM_KEY' in env.keys() else ""
 baseURL = r"http://api.openweathermap.org/data/2.5/weather"
 zipmatcher = re.compile(r"[0-9]{5}")
 selection = ''
@@ -18,5 +20,8 @@ while selection.lower() != 'q':
 	zipcode = zipmatcher.match(selection)
 	if not zipcode:
 		continue
-	zipcode = zipcode.group(0)
+	zipcode = str(zipcode.group(0))+",us"
+	parameters = {'zip': zipcode, 'APPID': apiKey}
+	response = requests.get(baseURL, params=parameters)
+	print(response.status_code)
 	print("The weather in {z} is ...".format(z=zipcode))
